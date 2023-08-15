5df4dd6fff4d22ea225e1835288a8cc733185002
JuliaFee <julia.silva133@aluno.senai.br>
2023-08-15 15:16:57 -0300

    feat: js tasks create

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,64 +1,65 @@
-
-let tasks = [];
-class task{
-    constructor(id, title, status){
-        this.id = id;
-        this.title = title;
-        this.status = status;
-    }
+class Task {
+  constructor(id, title, status) {
+    this.id = id;
+    this.title = title;
+    this.status = status;
+  }
 }
-function randomId(){
-    return  Math.floor(Math.random() * 9999);
+function randomId() {
+  return Math.floor(Math.random() * 9999);
 }
-class taskList{
-    constructor(){
-        this.tasks = [];
-    }
+class TaskList {
+  constructor() {
+    this.tasksArray = [];
+  }
 
-    addTask(task){
-        this.tasks.push(task);
-    }
+  addTask(task){
+    this.tasksArray.push(task);
+  }
+}
+
+const tasks = new TaskList();
+
+function createTask() {
+  let title = document.getElementById("areabox1").value;
+  let newTask = new Task(randomId(), title, false);
+
+  tasks.addTask(newTask);
+
+  document.getElementById("areabox1").value = "";
+  
+
+  showForm();
 }
 
 function showForm() {
-    let msg = "";
+  let msg = "";
 
-    msg += `<div id="box2">
-    <div class="form">
-        <div class="div-form">
-            <span id="task">${this.title}</span>
-        </div>
-       
-        <div class="btns">
-        <button type="button" id="verify" class="btn2">
-        <i class="fa-solid fa-check"></i>
-    </button>
-    <button type="button" id="edit" class="btn2">
-        <i class="fa-solid fa-pen"></i>
-    </button>
-    <button type="button" id="delete" class="btn2">
-        <i class="fa-solid fa-trash"></i>
-    </button>
-</div>
-</div>
-    </div>`
+
+  tasks.tasksArray.forEach(task => {
+    msg += `
+    <div id="box2">
+        <div class="form">
+            <div class="div-form">
+                <span id="task">${task.title}</span>
+            </div>
         
-    ;
+            <div class="btns">
+                <button type="button" id="verify" class="btn2">
+                    <i class="fa-solid fa-check"></i>
+                </button>
+                <button type="button" id="edit" class="btn2">
+                    <i class="fa-solid fa-pen"></i>
+                </button>
+                <button type="button" id="delete" class="btn2">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        </div>
+    </div>`;
+  })
 
-    document.getElementById("hidden").innerHTML = msg;
+  
+
+  document.getElementById("hidden").innerHTML = msg;
 }
-
-
-
-function createTask(){
-    let title = document.getElementById("areabox1").value;
-    let showTask = new task (randomId(), title, false);
-
-    
-    document.getElementById("areabox1").value = "";
-    console.log(showTask);
-
-
-    showForm();
-}
-
